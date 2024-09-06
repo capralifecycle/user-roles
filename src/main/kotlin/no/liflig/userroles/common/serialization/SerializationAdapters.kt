a1740c4f8790fc155ae0b625bcf74e24e46c60b4
hermannm <hermann.morkrid@gmail.com>
2024-09-06 08:56:01 +0200

    Remove now-redundant SerializationAdapters file

--- src/main/kotlin/no/liflig/userroles/common/serialization/SerializationAdapters.kt
+++
@@ -1,5 +0,0 @@
-package no.liflig.userroles.common.serialization
-
-import no.liflig.userroles.features.userroles.UserRole
-
-val userRolesSerializationAdapter = KotlinSerialization(UserRole.serializer())
