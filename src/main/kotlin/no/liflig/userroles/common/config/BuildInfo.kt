eb693a2cd313a94d64c6b4855ca760647272bae9
hermannm <hermann.morkrid@gmail.com>
2024-11-21 10:24:10 +0100

    Update POM and config to match REST service baseline

--- src/main/kotlin/no/liflig/userroles/common/config/BuildInfo.kt
+++ src/main/kotlin/no/liflig/userroles/common/config/BuildInfo.kt
@@ -3,7 +3,6 @@
 package no.liflig.userroles.common.config
 
 import java.time.Instant
-import java.time.format.DateTimeParseException
 import java.util.Properties
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
@@ -14,8 +13,7 @@ import no.liflig.userroles.common.serialization.InstantSerializer
 
 @Serializable
 data class BuildInfo(
-    /** During local development, this will be null. */
-    val timestamp: Instant?,
+    val timestamp: Instant,
     /** Git commit sha. */
     val commit: String,
     /** Git branch. */
@@ -29,12 +27,7 @@ data class BuildInfo(
     /** Create [BuildInfo] based on keys in `application.properties`. */
     fun from(properties: Properties) =
         BuildInfo(
-            timestamp =
-                try {
-                  Instant.parse(properties.stringNotNull("build.timestamp"))
-                } catch (ex: DateTimeParseException) {
-                  Instant.ofEpochMilli(0L)
-                },
+            timestamp = Instant.parse(properties.stringNotNull("build.timestamp")),
             commit = properties.stringNotNull("build.commit"),
             branch = properties.stringNotNull("build.branch"),
             number =
