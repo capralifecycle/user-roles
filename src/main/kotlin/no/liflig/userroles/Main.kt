cc3456fd194dff9ddb32368f3b901d0b7a500158
hermannm <hermann.morkrid@gmail.com>
2025-11-26 10:16:08 +0100

    Wrap main in try/catch for better error logging on startup failure

--- src/main/kotlin/no/liflig/userroles/Main.kt
+++ src/main/kotlin/no/liflig/userroles/Main.kt
@@ -1,7 +1,20 @@
 package no.liflig.userroles
 
+import kotlin.system.exitProcess
+import no.liflig.logging.getLogger
 import no.liflig.userroles.common.config.Config
 
+private val log = getLogger()
+
 fun main() {
-  App(Config.load()).start()
+  try {
+    App(Config.load()).start()
+  } catch (e: Throwable) {
+    /**
+     * If we failed to load config/start the application, we want to log it, to make sure that we
+     * don't lose the exception.
+     */
+    log.error(e) { "Application startup failed" }
+    exitProcess(status = 1)
+  }
 }
