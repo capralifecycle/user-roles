6f5cd398825f4251173ac197a4762b3f1a61f8ab
hermannm <hermann.morkrid@gmail.com>
2025-11-26 16:32:17 +0100

    Add exception printing fallback in UncaughtExceptionHandler

--- src/main/kotlin/no/liflig/userroles/Main.kt
+++ src/main/kotlin/no/liflig/userroles/Main.kt
@@ -8,18 +8,30 @@ private val log = getLogger()
 
 fun main() {
   /** Ensures that all uncaught exceptions are logged. */
-  Thread.setDefaultUncaughtExceptionHandler { _, e: Throwable ->
-    log.error(e) { "Uncaught exception in thread" }
-  }
+  Thread.setDefaultUncaughtExceptionHandler(UncaughtExceptionHandler)
 
   try {
     App(Config.load()).start()
   } catch (e: Throwable) {
-    /**
-     * If we failed to load config/start the application, we want to log it, to make sure that we
-     * don't lose the exception.
-     */
     log.error(e) { "Application startup failed" }
     exitProcess(status = 1)
+  }
+}
+
+private object UncaughtExceptionHandler : Thread.UncaughtExceptionHandler {
+  private val log = getLogger()
+
+  override fun uncaughtException(thread: Thread, exception: Throwable) {
+    try {
+      /** Logback includes a "thread_name" field in the log, so we can know which thread threw. */
+      log.error(exception) { "Uncaught exception in thread" }
+    } catch (_: Throwable) {
+      /**
+       * Fallback to default behavior of [ThreadGroup.uncaughtException], in case there's an error
+       * in our logger.
+       */
+      System.err.print("""Exception in thread "${thread.name}" """)
+      exception.printStackTrace(System.err)
+    }
   }
 }
